80464d5d1137ced6a0adf8fd399c7b3344d55bc6
Michael Nannola <michael.nannola@gmail.com>
2024-05-11 21:22:28 -0500

    Add new trip button and expedia link

--- app/routes/_marketing+/index.tsx
+++ app/routes/_marketing+/index.tsx
@@ -1,13 +1,27 @@
 import { type MetaFunction } from '@remix-run/node'
 import { Link } from '@remix-run/react'
+import { Button } from '#app/components/ui/button.js'
+import { Icon } from '#app/components/ui/icon.js'
 
 export const meta: MetaFunction = () => [{ title: 'Explorer Pals' }]
 
 export default function Index() {
 	return (
-		<main className="m-4">
-			<Link to="/trips">View your Trips</Link>
-
+		<main className="container mx-auto py-8">
+			<div className="my-3">
+				<Button asChild>
+					<Link to="/trips">View your Trips</Link>
+				</Button>			
+			</div>
+			
+			<div>
+				<Button variant="secondary" asChild>
+					<a href="https://expedia.com/affiliate/3yRyX67">
+						Go to Expedia! <Icon name="exit"></Icon></a>
+				</Button>
+			</div>
+			
+							
 		</main>
 	)
 }
--- app/routes/trips+/index.tsx
+++ app/routes/trips+/index.tsx
@@ -1,8 +1,9 @@
 import { invariantResponse } from '@epic-web/invariant'
 import { json, type LoaderFunctionArgs } from "@remix-run/node";
+import { Link, useLoaderData } from '@remix-run/react';
+import { Button } from '#app/components/ui/button.js';
 import { requireUserId } from "#app/utils/auth.server";
 import { prisma } from "#app/utils/db.server";
-import { Link, useLoaderData } from '@remix-run/react';
 
 export async function loader({ request }: LoaderFunctionArgs) {
     const userId = await requireUserId(request)
@@ -29,8 +30,11 @@ export async function loader({ request }: LoaderFunctionArgs) {
 export default function TripsRoute() {
     const data = useLoaderData<typeof loader>();
     return (
-        <main className="container mx-auto py-8">            
-             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        <main className="container mx-auto py-8">
+            <div className="my-3">
+                <Button asChild><Link to="/trips/new">Create a Trip</Link></Button>
+            </div>
+             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">                
                 {data.owner.trips.map(trip => (
                     <Link to={trip.id} prefetch="intent" key={trip.id}>
                         <div className="rounded-lg border bg-card text-card-foreground shadow-sm w-[350px]">
@@ -40,7 +44,7 @@ export default function TripsRoute() {
                             </div>
                         </div>
                     </Link>
-                ))}
+                ))}                
              </div>
         </main>
         
